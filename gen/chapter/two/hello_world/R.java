bf99a6adaf58510d9fd2018bb6676ebf56663159
Chris Fuller <chris@cjwfuller.com>
2013-12-13 15:13:19 +0000

    Chapter 13 complete

--- gen/chapter/two/hello_world/R.java
+++ gen/chapter/two/hello_world/R.java
@@ -23,8 +23,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int anim_effect_beam=0x7f020000;
@@ -95,55 +95,56 @@ public final class R {
         public static final int venus=0x7f020041;
     }
     public static final class id {
-        public static final int addPlanetText=0x7f090001;
-        public static final int basesButton=0x7f090010;
-        public static final int bombButton=0x7f090009;
-        public static final int coloniesButton=0x7f09000e;
-        public static final int colonistsButton=0x7f09000f;
-        public static final int dataView1=0x7f09001c;
-        public static final int dataView2=0x7f09001e;
-        public static final int dataView3=0x7f090020;
-        public static final int dataView4=0x7f090022;
-        public static final int dataView5=0x7f090024;
-        public static final int dataView6=0x7f090026;
-        public static final int dataView7=0x7f090028;
-        public static final int dataView8=0x7f09002a;
-        public static final int doneAddingButton=0x7f090002;
-        public static final int doneButton=0x7f090014;
-        public static final int editTextBases=0x7f090017;
-        public static final int editTextColonies=0x7f090015;
-        public static final int editTextColonists=0x7f090016;
-        public static final int editTextForcefieldOff=0x7f09001a;
-        public static final int editTextForcefieldOn=0x7f090019;
-        public static final int editTextMilitary=0x7f090018;
-        public static final int exitButton=0x7f09000d;
-        public static final int ffoffButton=0x7f090013;
-        public static final int ffonButton=0x7f090012;
-        public static final int imageEarth=0x7f090003;
-        public static final int imageJupiter=0x7f090006;
-        public static final int imageMars=0x7f090004;
-        public static final int imageNeptune=0x7f090008;
-        public static final int imageSaturn=0x7f090007;
-        public static final int imageVenus=0x7f090005;
-        public static final int infectButton=0x7f09000b;
-        public static final int invadeButton=0x7f09000a;
-        public static final int laserButton=0x7f09000c;
-        public static final int menu_add=0x7f09002c;
-        public static final int menu_attack=0x7f09002f;
-        public static final int menu_config=0x7f09002d;
-        public static final int menu_home=0x7f090030;
-        public static final int menu_travel=0x7f09002e;
-        public static final int militaryButton=0x7f090011;
-        public static final int new_planet_screen=0x7f090000;
-        public static final int textView1=0x7f09001b;
-        public static final int textView2=0x7f09001d;
-        public static final int textView3=0x7f09001f;
-        public static final int textView4=0x7f090021;
-        public static final int textView5=0x7f090023;
-        public static final int textView6=0x7f090025;
-        public static final int textView7=0x7f090027;
-        public static final int textView8=0x7f090029;
-        public static final int travelVideoView=0x7f09002b;
+        public static final int addPlanetText=0x7f0a0001;
+        public static final int basesButton=0x7f0a0010;
+        public static final int bombButton=0x7f0a0009;
+        public static final int coloniesButton=0x7f0a000e;
+        public static final int colonistsButton=0x7f0a000f;
+        public static final int dataView1=0x7f0a001c;
+        public static final int dataView2=0x7f0a001e;
+        public static final int dataView3=0x7f0a0020;
+        public static final int dataView4=0x7f0a0022;
+        public static final int dataView5=0x7f0a0024;
+        public static final int dataView6=0x7f0a0026;
+        public static final int dataView7=0x7f0a0028;
+        public static final int dataView8=0x7f0a002a;
+        public static final int doneAddingButton=0x7f0a0002;
+        public static final int doneButton=0x7f0a0014;
+        public static final int editTextBases=0x7f0a0017;
+        public static final int editTextColonies=0x7f0a0015;
+        public static final int editTextColonists=0x7f0a0016;
+        public static final int editTextForcefieldOff=0x7f0a001a;
+        public static final int editTextForcefieldOn=0x7f0a0019;
+        public static final int editTextMilitary=0x7f0a0018;
+        public static final int exitButton=0x7f0a000d;
+        public static final int ffoffButton=0x7f0a0013;
+        public static final int ffonButton=0x7f0a0012;
+        public static final int imageEarth=0x7f0a0003;
+        public static final int imageJupiter=0x7f0a0006;
+        public static final int imageMars=0x7f0a0004;
+        public static final int imageNeptune=0x7f0a0008;
+        public static final int imageSaturn=0x7f0a0007;
+        public static final int imageVenus=0x7f0a0005;
+        public static final int infectButton=0x7f0a000b;
+        public static final int invadeButton=0x7f0a000a;
+        public static final int laserButton=0x7f0a000c;
+        public static final int menu_add=0x7f0a002d;
+        public static final int menu_attack=0x7f0a0030;
+        public static final int menu_config=0x7f0a002e;
+        public static final int menu_home=0x7f0a0031;
+        public static final int menu_travel=0x7f0a002f;
+        public static final int militaryButton=0x7f0a0011;
+        public static final int new_planet_screen=0x7f0a0000;
+        public static final int textView1=0x7f0a001b;
+        public static final int textView2=0x7f0a001d;
+        public static final int textView3=0x7f0a001f;
+        public static final int textView4=0x7f0a0021;
+        public static final int textView5=0x7f0a0023;
+        public static final int textView6=0x7f0a0025;
+        public static final int textView7=0x7f0a0027;
+        public static final int textView8=0x7f0a0029;
+        public static final int travelButton=0x7f0a002c;
+        public static final int travelVideoView=0x7f0a002b;
     }
     public static final class layout {
         public static final int activity_add=0x7f030000;
@@ -153,56 +154,74 @@ public final class R {
         public static final int activity_travel=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_main=0x7f080000;
+        public static final int activity_main=0x7f090000;
+    }
+    public static final class raw {
+        public static final int ambient=0x7f050000;
+        public static final int ambients=0x7f050001;
+        public static final int blast=0x7f050002;
+        public static final int blast_flac=0x7f050003;
+        public static final int click=0x7f050004;
+        public static final int laser=0x7f050005;
+        public static final int mars=0x7f050006;
+        public static final int mars1080=0x7f050007;
+        public static final int mars270=0x7f050008;
+        public static final int mars480=0x7f050009;
+        public static final int mars720=0x7f05000a;
+        public static final int music=0x7f05000b;
+        public static final int musics=0x7f05000c;
+        public static final int transport=0x7f05000d;
+        public static final int virus=0x7f05000e;
     }
     public static final class string {
-        public static final int action_settings=0x7f060005;
-        public static final int activity_title_attack_planet=0x7f060004;
-        public static final int activity_title_config_planet=0x7f060002;
-        public static final int activity_title_new_planet=0x7f060001;
-        public static final int activity_title_travel_planet=0x7f060003;
-        public static final int add_planet_caption=0x7f060028;
-        public static final int app_name=0x7f060000;
-        public static final int attack_planet_bomb=0x7f06002a;
-        public static final int attack_planet_caption=0x7f060029;
-        public static final int attack_planet_exit=0x7f06002e;
-        public static final int attack_planet_infect=0x7f06002c;
-        public static final int attack_planet_invade=0x7f06002b;
-        public static final int attack_planet_laser=0x7f06002d;
-        public static final int button_name_bases=0x7f06001d;
-        public static final int button_name_colonies=0x7f06001b;
-        public static final int button_name_colonists=0x7f06001c;
-        public static final int button_name_done=0x7f060021;
-        public static final int button_name_done_add=0x7f060023;
-        public static final int button_name_ffoff=0x7f060020;
-        public static final int button_name_ffon=0x7f06001f;
-        public static final int button_name_military=0x7f06001e;
-        public static final int button_name_return=0x7f060022;
-        public static final int content_desc_bomb=0x7f060024;
-        public static final int content_desc_earth=0x7f060014;
-        public static final int content_desc_exit=0x7f06001a;
-        public static final int content_desc_invade=0x7f060025;
-        public static final int content_desc_jupiter=0x7f060017;
-        public static final int content_desc_laser=0x7f060027;
-        public static final int content_desc_mars=0x7f060015;
-        public static final int content_desc_neptune=0x7f060019;
-        public static final int content_desc_saturn=0x7f060018;
-        public static final int content_desc_venus=0x7f060016;
-        public static final int content_desc_virus=0x7f060026;
-        public static final int hello_world=0x7f060006;
-        public static final int menu_add_planet=0x7f06000f;
-        public static final int menu_attack_planet=0x7f060012;
-        public static final int menu_config_planet=0x7f060010;
-        public static final int menu_home_planet=0x7f060013;
-        public static final int menu_travel_planet=0x7f060011;
-        public static final int planet_bases_label=0x7f06000d;
-        public static final int planet_colonies_label=0x7f06000a;
-        public static final int planet_forcefield_label=0x7f06000e;
-        public static final int planet_gravity_label=0x7f060009;
-        public static final int planet_mass_label=0x7f060008;
-        public static final int planet_military_label=0x7f06000c;
-        public static final int planet_name_label=0x7f060007;
-        public static final int planet_population_label=0x7f06000b;
+        public static final int action_settings=0x7f070005;
+        public static final int activity_title_attack_planet=0x7f070004;
+        public static final int activity_title_config_planet=0x7f070002;
+        public static final int activity_title_new_planet=0x7f070001;
+        public static final int activity_title_travel_planet=0x7f070003;
+        public static final int add_planet_caption=0x7f070028;
+        public static final int app_name=0x7f070000;
+        public static final int attack_planet_bomb=0x7f07002b;
+        public static final int attack_planet_caption=0x7f07002a;
+        public static final int attack_planet_exit=0x7f07002f;
+        public static final int attack_planet_infect=0x7f07002d;
+        public static final int attack_planet_invade=0x7f07002c;
+        public static final int attack_planet_laser=0x7f07002e;
+        public static final int button_name_bases=0x7f07001d;
+        public static final int button_name_colonies=0x7f07001b;
+        public static final int button_name_colonists=0x7f07001c;
+        public static final int button_name_done=0x7f070021;
+        public static final int button_name_done_add=0x7f070023;
+        public static final int button_name_ffoff=0x7f070020;
+        public static final int button_name_ffon=0x7f07001f;
+        public static final int button_name_military=0x7f07001e;
+        public static final int button_name_return=0x7f070022;
+        public static final int content_desc_bomb=0x7f070024;
+        public static final int content_desc_earth=0x7f070014;
+        public static final int content_desc_exit=0x7f07001a;
+        public static final int content_desc_invade=0x7f070025;
+        public static final int content_desc_jupiter=0x7f070017;
+        public static final int content_desc_laser=0x7f070027;
+        public static final int content_desc_mars=0x7f070015;
+        public static final int content_desc_neptune=0x7f070019;
+        public static final int content_desc_saturn=0x7f070018;
+        public static final int content_desc_venus=0x7f070016;
+        public static final int content_desc_virus=0x7f070026;
+        public static final int hello_world=0x7f070006;
+        public static final int menu_add_planet=0x7f07000f;
+        public static final int menu_attack_planet=0x7f070012;
+        public static final int menu_config_planet=0x7f070010;
+        public static final int menu_home_planet=0x7f070013;
+        public static final int menu_travel_planet=0x7f070011;
+        public static final int planet_bases_label=0x7f07000d;
+        public static final int planet_colonies_label=0x7f07000a;
+        public static final int planet_forcefield_label=0x7f07000e;
+        public static final int planet_gravity_label=0x7f070009;
+        public static final int planet_mass_label=0x7f070008;
+        public static final int planet_military_label=0x7f07000c;
+        public static final int planet_name_label=0x7f070007;
+        public static final int planet_population_label=0x7f07000b;
+        public static final int travel_button_caption=0x7f070029;
     }
     public static final class style {
         /** 
@@ -226,10 +245,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
 }
--- src/chapter/two/hello_world/TravelPlanet.java
+++ src/chapter/two/hello_world/TravelPlanet.java
@@ -1,10 +1,12 @@
 package chapter.two.hello_world;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.Toast;
 import android.widget.VideoView;
 
@@ -14,14 +16,22 @@ public class TravelPlanet extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_travel);
+		Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.mars270);
 		VideoView travelVideo = (VideoView)findViewById(R.id.travelVideoView);
-		travelVideo.setOnTouchListener(new View.OnTouchListener() {
+		travelVideo.setVideoURI(videoUri);
+		MediaController videoMediaController = new MediaController(this);
+		videoMediaController.setAnchorView(travelVideo);
+		travelVideo.setMediaController(videoMediaController);
+		travelVideo.bringToFront();
+		travelVideo.requestFocus();
+		travelVideo.start();
+		Button travelButton = (Button)findViewById(R.id.travelButton);
+		travelButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				Toast.makeText(TravelPlanet.this, "Going home", Toast.LENGTH_SHORT).show();
+			public void onClick(View v) {
+				Toast.makeText(TravelPlanet.this, "Going Home", Toast.LENGTH_SHORT).show();
 				finish();
-				return true;
 			}
 		});
 	}
